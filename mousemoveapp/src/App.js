88ee3d1d21552fc301abce64d8dd630650d56987
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-22 13:27:25 -0800

    set intial state, wrote onMouseMove method, wrote tranform function

--- mousemoveapp/src/App.js
+++ mousemoveapp/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return {
+    transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
+  };
+}
+
 class App extends Component {
+  state = {
+    styleOne: {},
+    styleTwo: {}
+  };
+
+  onMouseMove = e => {
+    this.setState({
+      styleOne: transform(-e.clientX / e.clientY),
+      styleTwo: transform(e.clientX / e.clientY)
+    });
+  };
+  
   render() {
     return (
       <div >
-    
+        
       </div>
     );
   }
